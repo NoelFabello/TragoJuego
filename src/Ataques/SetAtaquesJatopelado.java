dc0017d3ab02ffae30d970e1e2e703808c07b5d1
Noel Fabello <noelfabello@gmail.com>
2020-08-10 11:29:54 +0200

    ostia añadida

--- src/Ataques/SetAtaquesJatopelado.java
+++ src/Ataques/SetAtaquesJatopelado.java
@@ -11,7 +11,7 @@ public class SetAtaquesJatopelado extends  ArrayList<Ataque>{
         this.add(new Ataque("Mar de Lagrimas","",110,100,30,0, 0,0,0, 0,0,0,0,0 ,Estados.ESTRESADO,null));
         this.add(new Ataque("Peste Otaku","",0,80,100,0, -20,0,0, 0,0,0,0,0 ,null,null));
         this.add(new Ataque("","",0,100,100,0, 0,0,0, 0,0,0,0,0 ,Estados.TILTEADO,null));
-        this.add(new Ataque("Colleja","",60,100,0,0, 0,0,0, 0,0,-10,0,0 ,null,null));
+        this.add(new Ataque("Ostia","",60,100,0,0, 0,0,0, 0,0,-10,0,0 ,null,null));
 
 
     }
