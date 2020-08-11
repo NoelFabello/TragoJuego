a6e9df41e0f30f02e6d951d3f1e133f02bef3492
Saúl <saulcandanvidal26@gmail.com>
2020-08-11 15:37:59 +0200

    Interfaz un poco hecha ya

--- src/Ilc/Control.java
+++ src/Ilc/Control.java
@@ -5,12 +5,34 @@
  */
 package Ilc;
 
+import Nucleo.Jugador;
+import Nucleo.Personaje;
+import Personajes.Miranda;
+import Personajes.Noel;
+import Personajes.Sulo;
+import java.util.ArrayList;
+
 /**
  *
  * @author saul
  */
 public class Control {
-    public static void Inicio(){
-        PartidaPrincipal p = new PartidaPrincipal();
+    public static void Inicio(){Personaje noel = new Noel();
+        Sulo sulo = new Sulo(); 
+        Miranda miranda = new Miranda();
+        Noel noel2 = new Noel();
+        Sulo sulo2 = new Sulo(); 
+        Miranda miranda2 = new Miranda();
+        ArrayList<Personaje> e1 = new ArrayList<>();
+        ArrayList<Personaje> e2 = new ArrayList<>();
+        e1.add(noel);
+        e1.add(sulo);
+        e1.add(miranda);
+        e1.add(noel2);
+        e1.add(sulo2);
+        e1.add(miranda2);
+        Jugador jugador1 = new Jugador("J1",e1,null);
+        Jugador jugador2 = new Jugador("J2",e2,null);
+        PartidaPrincipal p = new PartidaPrincipal(jugador1, jugador2);
     }
 }
--- src/Ilc/PartidaPrincipal.java
+++ src/Ilc/PartidaPrincipal.java
@@ -1,7 +1,13 @@
 package Ilc;
 
+import Interfaz.Lateral;
+import Interfaz.Mando;
+import Interfaz.PanelPrincipal;
 import Nucleo.Campo;
 import Nucleo.Jugador;
+import java.awt.Color;
+import static java.awt.Color.BLUE;
+import static java.awt.Color.red;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,17 +17,46 @@ public class PartidaPrincipal extends JFrame {
     private Jugador jugadorIzquierda;
     private Jugador jugadorDerecha;
     private Campo campo;
-    private JPanel principal;
+    private PanelPrincipal principal;
+    private Mando mandoIzquierda;
+    private Mando mandoDerecha;
+    private Lateral lateralIzquierdo;
+    private Lateral lateralDerecho;
+
     
-    public PartidaPrincipal() {
+    public PartidaPrincipal(Jugador jugador1, Jugador jugador2) {
+        
         super();
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(0, 0, 1938, 1048);
         this.setExtendedState(MAXIMIZED_BOTH);
         this.setIconImage(new ImageIcon("Fotos/FotoTrago.jfif").getImage());
         
-
+        InitComponents(jugador1,jugador2);
     }
+
+    private void InitComponents(Jugador jugador1, Jugador jugador2) {
+        
+        jugadorIzquierda = jugador1;
+        jugadorDerecha = jugador2;
+        
+        principal = new PanelPrincipal(jugadorIzquierda,jugadorDerecha,getWidth()*3/12, 0, getWidth()*6/12, getHeight()*2/3);
+        
+        mandoIzquierda = new Mando(jugadorIzquierda, 0, getHeight()*2/3, getWidth()/2, getHeight()/3);
+        mandoDerecha = new Mando(jugadorDerecha, getWidth()/2, getHeight()*2/3, getWidth()/2, getHeight()/3);
+        
+        lateralIzquierdo = new Lateral(0, 0, getWidth()*3/12, getHeight()*2/3);
+        lateralDerecho = new Lateral(getWidth()*9/12, 0, getWidth()*3/12, getHeight()*2/3);
+        
+        mandoIzquierda.setBackground(Color.yellow);
+        mandoDerecha.setBackground(Color.black);
+        
+        this.add(mandoIzquierda);
+        this.add(mandoDerecha);
+        this.add(principal);
+        this.add(lateralIzquierdo);
+        this.add(lateralDerecho);    }
     
     
     
